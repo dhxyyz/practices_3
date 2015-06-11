efe3fb605311481430a0548f34e243ea4ea7775b
dhxyyz <hd964660612@outlook.com>
2015-06-11 14:55:22 +0800

    用链式调用，重写了calculate_median_spec.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,22 +1,10 @@
 'use strict';
 var _ = require('../../../array/lodash');
 var calculate_median = function(collection){
-  var array = _.filter_in(collection,function(num, i){
+  var array = _(collection).filter(function(num, i){
     return i%2===1;
-  });
-  return _.median(array);
-  /*
-  var array = [];
-  for (var i = 0; i < collection.length; i++) {
-    if (i%2===1) {
-      array.push(collection[i]);
-    }
-  }
-  if (array.length%2===1) {
-    return array[(array.length-1)/2];
-  }else {
-    return (array[array.length/2]+array[array.length/2-1])/2;
-  }
-  */
+  }).value();
+  return _(array).median().value();
+
 };
 module.exports = calculate_median;
