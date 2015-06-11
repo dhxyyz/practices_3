a682f55e176c07e3b7c050519dd5175e7473002d
dhxyyz <hd964660612@outlook.com>
2015-06-11 14:53:36 +0800

    用链式调用，重写了rank_by_two_large_one_small.jscalculate_average_spec.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,18 +1,10 @@
 'use strict';
 var _ = require('../../../array/lodash');
 var calculate_average = function(collection){
-  var array = _.filter_in(collection,function(num,i){
+  var array = _(collection).filter(function(num,i){
     return i%2===1;
-  });
-  return _.mean(array);
-  /*
-  var number = 0;
-  var k = 0;
-  for (var i = 1; i < collection.length; i+=2) {
-    number = number + collection[i];
-    k++;
-  }
-  return number/k;
-  */
+  }).value();
+  return _(array).mean().value();
+
 };
 module.exports = calculate_average;
