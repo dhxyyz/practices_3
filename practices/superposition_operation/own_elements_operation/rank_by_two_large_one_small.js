617eeccefd29a0380826f8b46b62f5553fb1bf10
dhxyyz <hd964660612@outlook.com>
2015-06-11 14:50:01 +0800

    用链式调用，重写了rank_by_two_large_one_small.js

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,24 +1,15 @@
 'use strict';
 var _ = require('../../array/lodash');
 function rank_by_two_large_one_small(collection){
-  _.ranl(collection);
+
+  var array_a = _(collection).rank(function(a,b){
+    return a>b;
+  }).value();
   var array = [];
-/*
-  var array = [];
-  var number;
-  for (var i = 0; i < collection.length; i++) {
-    for (var j = i+1; j < collection.length; j++) {
-      if (collection[i]>collection[j]) {
-        number=collection[i];
-        collection[i]=collection[j];
-        collection[j]=number;
-      }
-    }
-  }
-*/
+  
   var box = 0;
-  for (var i = 0; i < collection.length; i++) {
-    array.push(collection[i]);
+  for (var i = 0; i < array_a.length; i++) {
+    array.push(array_a[i]);
     if (i%3===2) {
       box = array[i-2];
       array[i-2] = array[i-1];
