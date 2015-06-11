857b49194c9ed12b2717ae96820de234fd8139c3
dhxyyz <hd964660612@outlook.com>
2015-06-11 15:02:22 +0800

    用链式调用，重写了single_element_spec.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -2,20 +2,12 @@
 var _ = require('../../../array/lodash');
 var is_exist_element = function(collection,element){
   var judge = false;
-  _.each(collection,function(num,i){
+  _(collection).each(function(num,i){
     if (i%2===0&&collection[i]===element) {
       judge = true;
     }
   });
   return judge;
-  /*
-  var judge = false;
-  for (var i = 0; i < collection.length; i++) {
-    if (i%2===0&&collection[i]===element) {
-      judge = true;
-    }
-  }
-  return judge;
-  */
+
 };
 module.exports = is_exist_element;
